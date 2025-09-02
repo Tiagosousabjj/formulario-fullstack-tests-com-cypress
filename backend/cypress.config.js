52dc4e15cf2bcf51cf48029af621841669d32cce
Alexandre Tiago <134559870+Tiagosousabjj@users.noreply.github.com>
2025-09-02 04:11:39 -0300

    Update cypress.config.js

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -1,12 +1,13 @@
+// backend/cypress.config.js
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // eventos Node
+      // event listeners se precisar
     },
-    specPattern: "**/formulario.spec.js", // glob correto
-    supportFile: false,
+    specPattern: "formulario.spec.js", // caminho relativo ao backend/
+    supportFile: false,                 // se você não tiver suporte
     defaultCommandTimeout: 4000,
     pageLoadTimeout: 60000,
     browser: "chrome",
