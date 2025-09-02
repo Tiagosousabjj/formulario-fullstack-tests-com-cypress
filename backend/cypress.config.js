67045690d0295394d942dfe8a58488228bfff86c
Alexandre Tiago <134559870+Tiagosousabjj@users.noreply.github.com>
2025-09-02 02:34:05 -0300

    Update cypress.config.js

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
+const path = require("path");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    // caminho absoluto para os testes
+    specPattern: path.resolve(__dirname, "../cypress/e2e/**/*.cy.js"),
+    // caminho absoluto para o support file
+    supportFile: path.resolve(__dirname, "../cypress/support/e2e.js"),
   },
 });
