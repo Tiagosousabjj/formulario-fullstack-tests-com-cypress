e67725a18154d2aa5a8bb56f23b8fe838370959c
Alexandre Tiago <134559870+Tiagosousabjj@users.noreply.github.com>
2025-09-02 03:07:37 -0300

    Update cypress.config.js

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -4,14 +4,12 @@ const path = require("path");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // Node events podem ser adicionados aqui
+      // Event listeners, se precisar
     },
-    // Todos os testes .js dentro de cypress/e2e
-    specPattern: path.resolve(__dirname, "../cypress/e2e/**/*.js"),
-    // Caminho do support file
+    // Somente os testes do seu projeto
+    specPattern: path.resolve(__dirname, "../cypress/e2e/formulario/**/*.js"),
     supportFile: path.resolve(__dirname, "../cypress/support/e2e.js"),
-    // Timeout padrão esperado pelo teste
-    defaultCommandTimeout: 4000,
+    defaultCommandTimeout: 4000, // como esperado pelos testes
     pageLoadTimeout: 60000,
     browser: "chrome",
   },
